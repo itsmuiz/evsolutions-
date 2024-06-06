9b6f575aa272857107590dff2444307c067719ea
itsmuiz <pvt.muiz1012@gmail.com>
2024-06-06 22:12:53 +0530

    designing completed

---
+++ script.js
@@ -0,0 +1,29 @@
+let sideBar = document.querySelector('.side-bar');
+let navBar = document.querySelector('nav');
+let menu = document.querySelector('.icon');
+
+menu.addEventListener('click', () => {
+    if(sideBar.style.right === '0%'){
+        sideBar.style.right = '-100%';
+        menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+    }else{
+        sideBar.style.right = '0%';
+        menu.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+    }
+});
+
+
+const slider = document.querySelector('.slider');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', function () {
+    const slides = document.querySelectorAll('.slides');
+
+    slider.appendChild(slides[0]);
+});
+prev.addEventListener('click', function () {
+    const slides = document.querySelectorAll('.slides');
+
+    slider.prepend(slides[slides.length - 1]);
+});
