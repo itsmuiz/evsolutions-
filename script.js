efee52d40677f0e78f326f1105044aeeb4d245b0
itsmuiz <pvt.muiz1012@gmail.com>
2024-06-07 22:03:19 +0530

    sidebar hiden

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ let navBar = document.querySelector('nav');
 let menu = document.querySelector('.icon');
 
 menu.addEventListener('click', () => {
-    if(sideBar.style.display = 'none'){
+    if(sideBar.style.right === '0%'){
         sideBar.style.right = '-100%';
-        sideBar.style.display = 'block';
-        menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
-    }else{
         sideBar.style.display = 'none';
-        sideBar.style.right = '0%';
+        menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+        }else{
+            sideBar.style.right = '0%';
+            sideBar.style.display = 'block';
         menu.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
     }
 });
+
 
 
 const slider = document.querySelector('.slider');
