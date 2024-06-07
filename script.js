af4a17b5c2feb837440bb0c07355bedec1f37147
Thorfin <149878099+itsmuiz@users.noreply.github.com>
2024-06-07 19:01:06 +0530

    if els menu script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let navBar = document.querySelector('nav');
 let menu = document.querySelector('.icon');
 
 menu.addEventListener('click', () => {
-    if(sideBar.style.right === '0%'){
+    if(sideBar.style.display = 'none'){
         sideBar.style.right = '-100%';
-        sideBar.style.display = 'none';
+        sideBar.style.display = 'block';
         menu.innerHTML = `<i class="fa-solid fa-bars"></i>`;
     }else{
-        sideBar.style.display = 'block';
+        sideBar.style.display = 'none';
         sideBar.style.right = '0%';
         menu.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
     }
