27cba9ac40b3d7b08790c777b8456797385c2ee0
itsmuiz <pvt.muiz1012@gmail.com>
2024-12-18 19:01:59 +0530

    details added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// for navbar
 let sideBar = document.querySelector('.side-bar');
 let navBar = document.querySelector('nav');
 let menu = document.querySelector('.icon');
@@ -15,7 +16,7 @@ menu.addEventListener('click', () => {
 });
 
 
-
+// home page
 const slider = document.querySelector('.slider');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
@@ -31,11 +32,12 @@ prev.addEventListener('click', function () {
     slider.prepend(slides[slides.length - 1]);
 });
 
+
 // the code for on scroll transition
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        console.log('entry: ', entry);
+        // console.log('entry: ', entry);
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
@@ -43,3 +45,47 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+// for fetch products data
+
+let cards = document.querySelector('.cards');
+
+fetch('../src/products.json')
+    .then(response => response.text())
+    .then(data => {
+        const products = JSON.parse(data);
+        // console.log(products);
+        products.forEach(product => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            image = product.name + '.jpg';
+
+            const filePath = `../src/${image}`;
+
+            fetch(filePath, { method: 'HEAD' }) // Use 'HEAD' to get metadata only
+                .then(response => {
+                    if (response !== response.ok) {
+                        image = 'default.png';
+                    }})
+                .catch(error => {
+                    console.log(error);
+                });
+
+
+            card.innerHTML = `
+                 <div class="card-img">
+                     <img src="../src/${image}" alt="image not found">
+                 </div>
+                 <p>${product.price} INR</p>
+                 <p>${product.name}</p>
+                 <span>
+                     <a href="#" target="_blank">
+                         <i class="fa fa-shopping-cart"></i> Add to cart
+                     </a>
+                 </span>
+            `;
+            cards.appendChild(card);
+        });
+    });
